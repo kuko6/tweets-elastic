7ddf6603af522d761abbe6c3a9194dcd936c6061
Jakub Povinec <jakub.povinec@gmail.com>
2022-12-22 18:58:05 +0100

    working import

--- src/main.py
+++ src/main.py
@@ -33,133 +33,131 @@ def create_index(es: Elasticsearch) -> None:
     print(f'Created new index {INDEX_NAME}')
 
 
-def fetch_data(conn: psycopg.Connection) -> list[dict]:
-    pass
-
-
-def import_data(conn: psycopg.Connection, es: Elasticsearch) -> None:
-    with conn.cursor(name='tweets') as cur:
-        # cur.itersize = 10000
-        cur.execute("""
+def fetch_data(conn: psycopg.Connection, next_id=0, limit=5000) -> psycopg.ServerCursor:
+    cur = conn.cursor(name='tweets')
+    cur.itersize = 10000
+    cur.execute("""
+    SELECT 
+        c.id, c."content", c.possibly_sensitive, c."language", c."source", c.retweet_count, c.reply_count, c.like_count, c.quote_count, c.created_at,
+        to_json(a.*) author,
+        COALESCE(ca.jsons, '[]') context_annotations,
+        COALESCE(ch.jsons, '[]') conversation_hashtags,
+        COALESCE(an.jsons, '[]') annotations,
+        COALESCE(l.jsons, '[]') links,
+        COALESCE(cr.jsons, '[]') conversation_references
+    FROM conversations c
+    JOIN authors a ON c.author_id = a.id
+    LEFT JOIN (
+        SELECT ca.conversation_id, json_agg(json_build_object('entity', ce.*, 'domain', cd.*)) jsons
+        FROM context_annotations ca
+        JOIN context_entities ce ON ca.context_entity_id = ce.id
+        JOIN context_domains cd ON ca.context_domain_id = cd.id 
+        GROUP BY ca.conversation_id
+    ) ca ON ca.conversation_id = c.id
+    LEFT JOIN (
+        SELECT ch.conversation_id, json_agg(json_build_object('tag', h.tag)) jsons 
+        FROM conversation_hashtags ch
+        JOIN hashtags h ON ch.hashtag_id = h.id
+        GROUP BY ch.conversation_id
+    ) ch ON ch.conversation_id = c.id
+    LEFT JOIN (
+        SELECT an.conversation_id, json_agg(json_build_object('value', an."value", 'probability', an.probability, 'type', an."type")) jsons 
+        FROM annotations an
+        GROUP BY an.conversation_id
+    ) an ON an.conversation_id = c.id
+    LEFT JOIN (
+        SELECT l.conversation_id, json_agg(json_build_object('url', l.url, 'title', l.title, 'description', l.description)) jsons 
+        FROM links l
+        GROUP BY l.conversation_id
+    ) l ON l.conversation_id = c.id
+    LEFT JOIN (
         SELECT 
-            c.id, c."content", c.possibly_sensitive, c."language", c."source", c.retweet_count, c.reply_count, c.like_count, c.quote_count, c.created_at,
-            to_json(a.*) author,
-            COALESCE(ca.jsons, '[]') context_annotations,
-            COALESCE(ch.jsons, '[]') conversation_hashtags,
-            COALESCE(an.jsons, '[]') annotations,
-            COALESCE(l.jsons, '[]') links,
-            COALESCE(cr.jsons, '[]') conversation_references
-        FROM conversations c
-        JOIN authors a ON c.author_id = a.id
-        LEFT JOIN (
-            SELECT ca.conversation_id, json_agg(json_build_object('entity', ce.*, 'domain', cd.*)) jsons
-            FROM context_annotations ca
-            JOIN context_entities ce ON ca.context_entity_id = ce.id
-            JOIN context_domains cd ON ca.context_domain_id = cd.id 
-            GROUP BY ca.conversation_id
-        ) ca ON ca.conversation_id = c.id
-        LEFT JOIN (
-            SELECT ch.conversation_id, json_agg(json_build_object('tag', h.tag)) jsons 
-            FROM conversation_hashtags ch
-            JOIN hashtags h ON ch.hashtag_id = h.id
-            GROUP BY ch.conversation_id
-        ) ch ON ch.conversation_id = c.id
-        LEFT JOIN (
-            SELECT an.conversation_id, json_agg(json_build_object('value', an."value", 'probability', an.probability, 'type', an."type")) jsons 
-            FROM annotations an
-            GROUP BY an.conversation_id
-        ) an ON an.conversation_id = c.id
-        LEFT JOIN (
-            SELECT l.conversation_id, json_agg(json_build_object('url', l.url, 'title', l.title, 'description', l.description)) jsons 
-            FROM links l
-            GROUP BY l.conversation_id
-        ) l ON l.conversation_id = c.id
-        LEFT JOIN (
-            SELECT 
-                cr.conversation_id,
-                json_agg(json_build_object(
-                    'id', p.id, 'type', cr."type", 'content', p."content",
-                    'author', (
-                        SELECT json_build_object('id', pa.id, 'name', pa."name", 'username', pa.username) 
-                        FROM authors pa 
-                        WHERE pa.id = p.author_id
-                    ),
-                    'hashtags', (
-                        SELECT json_agg(json_build_object('tag', h.tag)) 
-                        FROM conversation_hashtags ch 
-                        JOIN hashtags h ON ch.hashtag_id = h.id
-                        WHERE ch.conversation_id = p.id
-                    )
-                )) jsons
-            FROM conversation_references cr
-            JOIN conversations p ON cr.parent_id = p.id
-            GROUP BY cr.conversation_id
-        ) cr ON cr.conversation_id = c.id
-        -- WHERE c.id = '1496733334587777024';
-        LIMIT 1000000;
-        """)
-        
+            cr.conversation_id,
+            json_agg(json_build_object(
+                'id', p.id, 'type', cr."type", 'content', p."content",
+                'author', (
+                    SELECT json_build_object('id', pa.id, 'name', pa."name", 'username', pa.username) 
+                    FROM authors pa 
+                    WHERE pa.id = p.author_id
+                ),
+                'hashtags', (
+                    SELECT json_agg(json_build_object('tag', h.tag)) 
+                    FROM conversation_hashtags ch 
+                    JOIN hashtags h ON ch.hashtag_id = h.id
+                    WHERE ch.conversation_id = p.id
+                )
+            )) jsons
+        FROM conversation_references cr
+        JOIN conversations p ON cr.parent_id = p.id
+        GROUP BY cr.conversation_id
+    ) cr ON cr.conversation_id = c.id
+    WHERE c.id > (%s)
+    LIMIT (%s);
+    """, (next_id, limit))
+    
+    return cur
+
+
+def import_data(es: Elasticsearch, data_size=-1) -> None:
+    batch_size = 1000
+    limit = 100000
+
+    total_time = start_time = time.time()
+    total_processed_rows = 0
+    
+    if data_size != -1 and data_size < limit:
+        limit = data_size
+
+    next_id = 0
+    while True: 
+        conn = connect_postgres()
+        if conn == None:
+            print('Connection to the database failed :(')
+            return
+
+        cur = fetch_data(conn, next_id, limit)
+
         data = []
         start_time = time.time()
-        j = 0
-        while True:
-            rows = cur.fetchmany(10000)
-            if len(rows) == 0:
-                break
+        processed_rows = 0
 
-            i = 0
-            data.clear()
-            # print('|New batch|')
-            for row in rows:
-                header = {'index': {'_index': INDEX_NAME, '_id': row.pop('id')}}
-                data.extend([header, row])
-                i += 1
-                j += 1
-                if j%100000 == 0:
-                    #print(i)
-                    print(f'Execution after {j} rows: {round(time.time() - start_time, 3)}s')
-
-        # i = 0
-        # for row in cur:
-        #     header = {'index': {'_index': INDEX_NAME, '_id': row.pop('id')}}
-        #     data.extend([header, row])
-        #     #print(len(row))
-        #     i += 1
-        #     if i%100000==0: 
-        #         #print(i)
-        #         print(f'Execution after {i} rows: {round(time.time() - start_time, 3)}s')
-        #         data.clear()
+        # iterate over the results 
+        for row in cur:
+            header = {'index': {'_index': INDEX_NAME, '_id': row.pop('id')}}
+            data.extend([header, row])
+            processed_rows += 1
+            if processed_rows%batch_size == 0:
+                next_id = header['index']['_id']
+                print(f'Execution after {processed_rows} rows: {round(time.time() - start_time, 3)}s')
+                a = es.bulk(index=INDEX_NAME, operations=data)
+                if a['errors']: 
+                    print(a['items'])
+                else: 
+                    print(f'Succesfully inserted: {len(data)/2}')
+                data.clear()
         
-        # print(data)
-        # a = es.bulk(index=INDEX_NAME, operations=data)
-        # if a['errors']: 
-        #     print(a['items'])
-        # else: 
-        #     print(f'Succesfully inserted: {len(rows)}')
+        total_processed_rows += processed_rows
+        print(f'Total execution after {total_processed_rows} rows: {round(time.time() - total_time, 3)}s') 
 
-        # rows[0]['created_at'] = str(rows[0]['created_at'])
-        # print(json.dumps(rows[0]))
-    input()    
-    conn.close()
-    input()
+        cur.close()
+        conn.close()
+        print(next_id)
+
+        if processed_rows == 0 or total_processed_rows >= data_size:
+            break
 
 
 def main() -> None:
-    conn = connect_postgres()
-    if conn == None:
-        print('Connection to the database failed :(')
+    es = connect_elastic()
+    if es == None:
+        print('Connection to Elastic failed :(')
         return
     
-    # es = connect_elastic()
-    # if es == None:
-    #     print('Connection to Elastic failed :(')
-    #     return
+    create_index(es)
+    import_data(es, data_size=5000)
     
-    #create_index(es)
-    import_data(conn, None)
-    
-    conn.close()
-    #es.close()
+    #conn.close()
+    es.close()
 
 
 if __name__ == '__main__':
